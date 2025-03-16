a96be0bb50eb5ed9177da7db12a58bdd93460ac8
Thomas Wang <tw864@drexel.edu>
2025-03-16 17:59:29 -0400

    serpapi config

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,6 @@ import preprocess from 'svelte-preprocess';
 export default {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '' : ''
-    }
   },
   preprocess: preprocess()
 };
