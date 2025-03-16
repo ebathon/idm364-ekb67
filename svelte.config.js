9a76f6bb3424e7f46a0e56aecd38a911cc2b701f
Thomas Wang <tw864@drexel.edu>
2025-03-16 18:01:23 -0400

    api folder error

--- svelte.config.js
+++ svelte.config.js
@@ -3,14 +3,8 @@ import preprocess from 'svelte-preprocess';
 
 
 export default {
-  kit: {
-    adapter: adapter(),
-<<<<<<< HEAD
-=======
-    alias: {
-      $lib: './src/lib'
-    }
->>>>>>> 2cd0e63aeecf5af7f0476ad752ee5f0e7bace0ac
-  },
-  preprocess: preprocess()
-};
+	kit: {
+		adapter: adapter()
+	  },
+	  preprocess: preprocess()
+	};
