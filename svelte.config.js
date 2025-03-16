2759d125760f049670fb2d84e8310f2423583a2e
ebathon <ekb67@drexel.edu>
2025-03-16 17:58:43 -0400

    Updated svelte.config.js
    Removes unnecessary paths.base that doesn’t affect deployment.
    Ensures $lib imports are resolved properly, avoiding module issues.

--- svelte.config.js
+++ svelte.config.js
@@ -3,8 +3,8 @@ import adapter from '@sveltejs/adapter-netlify';
 export default {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '' : ''
+    alias: {
+      $lib: './src/lib'
     }
   }
 };
