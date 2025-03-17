bbf31507ca9cbb3a85c0aa1d8bde0008124083aa
Thomas Wang <tw864@drexel.edu>
2025-03-16 21:43:32 -0400

    redirectss

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,8 @@
 import adapter from '@sveltejs/adapter-netlify';
-// Fix the import - use the correct location based on your SvelteKit version
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// OR if using an older version, you might not need vitePreprocess at all
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Preprocess your Svelte components
-  preprocess: vitePreprocess(), // Remove this line if import isn't available
-  
   kit: {
-    // Adapter for Netlify
     adapter: adapter()
   }
 };
