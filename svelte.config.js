6e48120c56473b653ec4c9505646ff68dc411a01
Thomas Wang <tw864@drexel.edu>
2025-03-16 21:27:35 -0400

    404 err fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
 import adapter from '@sveltejs/adapter-netlify';
-import { sveltePreprocess }  from 'svelte-preprocess';
-
 
 export default {
-	kit: {
-		adapter: adapter({})
-	  },
-	  preprocess: { sveltePreprocess }
-		};
+  kit: {
+    adapter: adapter()
+  }
+};
